ea222a9dc7b1c2f3828358db3db2e9c71fdde3f3
wrs0809 <williamsacaca92@gmail.com>
2023-11-11 20:20:46 -0300

    actualizacion de formularios

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
 })
 
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+})
+
 app.use(express.static("public"));
 
 app.listen (3500,()=>{console.log("servidor en ejecución puerto 3500")})
