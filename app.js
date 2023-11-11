eeb8404618a391cc59a59cbafec216ef4576966c
wrs0809 <williamsacaca92@gmail.com>
2023-11-11 20:28:41 -0300

    actualizacion boton enviar a home

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.get("/",(req,res)=>{
+app.get("/index",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
 })
 
